df7fe15a2e1cc356b3d064a2b9a6e7de03011079
rajanchoksi79 <164361171+rajanchoksi79@users.noreply.github.com>
2025-05-20 13:01:50 +0530

    third change made to project

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,3 @@
-/*
-
-This is implementation of cat program of linux in Rust.
-so basically it will read file if exists of any given directory, provided filename as argument and print the content of it.
-
-Author: Rajan Choksi
-Date: 19/05/2025
-
-*/
-
 // importing neccessary modules.
 use std::env;
 use std::fs::File;
@@ -19,13 +9,7 @@ fn main() -> Result<()> {
     let arguments: Vec<String> = env::args().collect();
 
     // this applies only if user has provided one argument, for only one file.
-    if arguments.len() < 2 {
-        println!("");
-        println!("------------------");
-        println!("Start of the file");
-        println!("------------------");
-        println!("");
-
+    if arguments.len() <= 2 {
         // using file path provided by the user.
         let file_path = arguments
             .get(1)
@@ -37,50 +21,75 @@ fn main() -> Result<()> {
         // reading file with bufferreader
         let reader = BufReader::new(file);
 
+        // displaying information about starting of file.
+        println!("\n------------------");
+        println!("Start of the file");
+        println!("------------------\n");
+
         // printing file line by line using for loop.
         for line in reader.lines() {
-            println!("{}", line?);
+            match line {
+                Ok(content) => println!("{}", content),
+                Err(e) => {
+                    eprintln!("failed to read from {}: {}", file_path, e);
+                    break;
+                }
+            }
         }
 
-        println!("");
-        println!("------------------");
+        // displaying information about ending of file.
+        println!("\n------------------");
         println!("End of the file");
-        println!("------------------");
-        println!("");
+        println!("------------------\n");
     }
     // this applies if user has provided more than one arguments, for reading more than one file.
-    else if arguments.len() > 1 {
+    else if arguments.len() > 2 {
         // applying for loop to read files, one after another.
         for i in 1..arguments.len() {
-            // displaying information about starting of file with it's number.
-            println!("");
-            println!("------------------");
-            println!("File number - {i}");
-            println!("------------------");
-            println!("");
-
             // using file path provided by user
-            let file_path = arguments
-                .get(i)
-                .expect("no file path or incorrect file path is provided");
+            let file_path = match arguments.get(i) {
+                Some(path) => path,
+                None => {
+                    eprintln!("missing or invalid file at file number - {}", i);
+                    continue;
+                }
+            };
 
             // opening the file if exists
-            let file = File::open(file_path).expect("file do not exists, try again");
+            let file = match File::open(file_path) {
+                Ok(o) => o,
+                Err(e) => {
+                    eprintln!(
+                        "failed to open file number - {} at given path - {}, {}",
+                        i, file_path, e
+                    );
+                    continue;
+                }
+            };
 
             // reading the file with bufferreader
             let reader = BufReader::new(file);
 
+            // displaying information about starting of file with it's number.
+            println!("\n-----------------------");
+            println!("Start of the file - {i}");
+            println!("-----------------------\n");
+
             // printing file line by line using for loop.
             for line in reader.lines() {
-                println!("{}", line?);
+                match line {
+                    Ok(content) => println!("{}", content),
+                    Err(e) => {
+                        eprintln!("failed to read from {}: {}", file_path, e);
+                        break;
+                    }
+                }
             }
 
             // displaying information about ending of file with it's number.
-            println!("");
-            println!("------------------");
-            println!("End of file - {i}");
-            println!("------------------");
-            println!("");
+            println!("\n-----------------------");
+            println!("End of the file - {i}");
+            println!("-----------------------\n");
         }
     }
 
